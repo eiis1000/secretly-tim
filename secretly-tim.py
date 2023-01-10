5e6372a4d10ad32804d70a69281a4c31809fe417
Erez Israeli Miller <erez.m.israeli@gmail.com>
2023-01-09 21:56:05 -0500

    Go back to 5 minutes for keyconfess prikey

--- secretly-tim.py
+++ secretly-tim.py
@@ -186,7 +186,7 @@ async def keyconfess(ctx):
     prikey = hexit(key.d) + '_' + hexit(key.p) + '_' + hexit(key.q)
     cnum = ginct(personal_ads.id)
     await personal_ads.send(f'**#{cnum}**: {parts[1]} | keyhash = {shorthash(pubkey)}, pubkey = {pubkey}')
-    await sendlogsleepdelete(ctx, parts[0], cnum, 60*60*24, True, f'Here is your private key:\n||{prikey}||\n**Make sure to save it somewhere safe and keep it secret!** For your own security, please delete your message. This message will self-destruct in 1 day. **If you do not save your private key, you will not be able to decrypt replies.**')
+    await sendlogsleepdelete(ctx, parts[0], cnum, 300, True, f'Here is your private key:\n||{prikey}||\n**Make sure to save it somewhere safe and keep it secret!** For your own security, please delete your message. This message will self-destruct in **5 minutes**. **If you do not save your private key, you will not be able to decrypt replies.**')
 
 @bot.command()
 async def encryptconfess(ctx):
