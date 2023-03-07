1441053ce23521c39b28fa78d17eec9268c9deb4
Erez Abrams <erez.m.israeli@gmail.com>
2023-03-06 22:18:31 -0500

    Update secretly-tim.py

--- secretly-tim.py
+++ secretly-tim.py
@@ -210,7 +210,7 @@ async def keyconfess(ctx):
     prikey = hexit(key.d) + '_' + hexit(key.p) + '_' + hexit(key.q)
     cnum = ginct(personal_ads.id)
     await personal_ads.send(f'**#{cnum}** as **{shorthash(pubkey)}**: {parts[1]} | keyhash = {shorthash(pubkey)}, pubkey = ||{pubkey}||')
-    await sendlogsleepdelete(ctx, parts[0], cnum, 300, True, f'Here is your private key:\n||{prikey}||\n**Make sure to save it somewhere safe and keep it secret!** For your own security, please delete your message. This message will self-destruct in **5 minutes**. **If you do not save your private key, you will not be able to decrypt replies.**')
+    await sendlogsleepdelete(ctx, parts[0], cnum, 300, True, f'Your private key is in the following spoilers. **Make sure to save it somewhere safe and keep it secret!**\n||{prikey}||\nFor your own security, please delete your message. This message will self-destruct in **5 minutes**. **If you do not save your private key, you will not be able to decrypt replies.**')
 
 @bot.command()
 async def encryptconfess(ctx):
