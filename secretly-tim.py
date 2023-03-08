95b55439f5c447092602f29122ab8ba2273a58e8
Erez Abrams <erez.m.israeli@gmail.com>
2023-03-07 20:25:29 -0500

    update version number

--- secretly-tim.py
+++ secretly-tim.py
@@ -160,7 +160,7 @@ async def on_message(message):
         if 'my son' in message.content.lower():
             await message.channel.send('yes father')
         if message.content.lower() == 'version':
-            await message.channel.send('1.1.5')
+            await message.channel.send('1.1.6')
         if message.content.lower() == 'kill yourself' or message.content.lower() == 'kys':
             await message.channel.send('okay :(')
             try:
