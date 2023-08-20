f7c1d38114a93266f82c2dcb18a673f99567a6fd
Erez Abrams <erez.m.israeli@gmail.com>
2023-08-20 16:23:07 -0400

    Fix parenthesis

--- secretly-tim.py
+++ secretly-tim.py
@@ -111,7 +111,7 @@ async def logconfess(num: int, confessor: discord.User, ctype: str):
 
 async def sendlogsleepdelete(ctx, type, cnum, time, logme, msg):
     try:
-        reply = await ctx.send(msg.replace('@','')
+        reply = await ctx.send(msg.replace('@', ''))
         if logme:
             await logconfess(cnum, ctx.author, type)
         to_delete[reply.id] = reply
